3b53c0f6b19a7eb204c3976e8e88f205bb9bb823
kaneha22898 <kaneha22898@gmail.com>
2024-05-02 13:54:18 +0700

    feat: update middleware to apply multi tenant domain

--- app/app/private/component.tsx
+++ app/app/private/component.tsx
@@ -3,7 +3,14 @@
 import { useUser } from '@cabin-id/nextjs';
 
 export const ClientComponent = () => {
-  const user = useUser();
-  // console.log('ClientComponent', user);
-  return <div>This is Client Component {JSON.stringify(user, null, 4)}</div>;
+  const { user, signOut } = useUser();
+  return (
+    <div>
+      <p>This is Client Component</p>
+      <p>{JSON.stringify(user, null, 4)}</p>
+      <button className="p-4 bg-violet-300" onClick={() => signOut()}>
+        SignOut
+      </button>
+    </div>
+  );
 };
